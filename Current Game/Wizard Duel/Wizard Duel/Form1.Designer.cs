ce293d0a9ab312e9680e46ba78617692e21bc242
Dustin Work <Dustin Work>
2017-04-25 12:47:49 -0500

    renamed all buttons:

--- Current Game/Wizard Duel/Wizard Duel/Form1.Designer.cs
+++ Current Game/Wizard Duel/Wizard Duel/Form1.Designer.cs
@@ -28,85 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.startCampaignBtn = new System.Windows.Forms.Button();
+            this.startArenaBtn = new System.Windows.Forms.Button();
+            this.startVersusBtn = new System.Windows.Forms.Button();
+            this.startChaosBtn = new System.Windows.Forms.Button();
+            this.optionsBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // startCampaignBtn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "CAMPAIGN";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.startCampaignBtn.Location = new System.Drawing.Point(12, 12);
+            this.startCampaignBtn.Name = "startCampaignBtn";
+            this.startCampaignBtn.Size = new System.Drawing.Size(223, 23);
+            this.startCampaignBtn.TabIndex = 0;
+            this.startCampaignBtn.Text = "CAMPAIGN";
+            this.startCampaignBtn.UseVisualStyleBackColor = true;
+            this.startCampaignBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // startArenaBtn
             // 
-            this.button2.Location = new System.Drawing.Point(12, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(223, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "ARENA";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.startArenaBtn.Location = new System.Drawing.Point(12, 41);
+            this.startArenaBtn.Name = "startArenaBtn";
+            this.startArenaBtn.Size = new System.Drawing.Size(223, 23);
+            this.startArenaBtn.TabIndex = 1;
+            this.startArenaBtn.Text = "ARENA";
+            this.startArenaBtn.UseVisualStyleBackColor = true;
+            this.startArenaBtn.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // startVersusBtn
             // 
-            this.button3.Location = new System.Drawing.Point(12, 70);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(223, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "VERSUS";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.startVersusBtn.Location = new System.Drawing.Point(12, 70);
+            this.startVersusBtn.Name = "startVersusBtn";
+            this.startVersusBtn.Size = new System.Drawing.Size(223, 23);
+            this.startVersusBtn.TabIndex = 2;
+            this.startVersusBtn.Text = "VERSUS";
+            this.startVersusBtn.UseVisualStyleBackColor = true;
+            this.startVersusBtn.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // startChaosBtn
             // 
-            this.button4.Location = new System.Drawing.Point(12, 99);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(223, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "CHAOS MODE";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.startChaosBtn.Location = new System.Drawing.Point(12, 99);
+            this.startChaosBtn.Name = "startChaosBtn";
+            this.startChaosBtn.Size = new System.Drawing.Size(223, 23);
+            this.startChaosBtn.TabIndex = 3;
+            this.startChaosBtn.Text = "CHAOS MODE";
+            this.startChaosBtn.UseVisualStyleBackColor = true;
+            this.startChaosBtn.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // optionsBtn
             // 
-            this.button5.Location = new System.Drawing.Point(12, 128);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(223, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "OPTIONS";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.optionsBtn.Location = new System.Drawing.Point(12, 128);
+            this.optionsBtn.Name = "optionsBtn";
+            this.optionsBtn.Size = new System.Drawing.Size(223, 23);
+            this.optionsBtn.TabIndex = 4;
+            this.optionsBtn.Text = "OPTIONS";
+            this.optionsBtn.UseVisualStyleBackColor = true;
+            this.optionsBtn.Click += new System.EventHandler(this.button5_Click);
             // 
-            // button6
+            // exitBtn
             // 
-            this.button6.Location = new System.Drawing.Point(12, 157);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(223, 23);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "EXIT GAME";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.exitBtn.Location = new System.Drawing.Point(12, 157);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(223, 23);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "EXIT GAME";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(247, 188);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.optionsBtn);
+            this.Controls.Add(this.startChaosBtn);
+            this.Controls.Add(this.startVersusBtn);
+            this.Controls.Add(this.startArenaBtn);
+            this.Controls.Add(this.startCampaignBtn);
             this.Name = "Form1";
             this.Text = "WIZARD DUEL";
             this.TopMost = true;
@@ -117,12 +117,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button startCampaignBtn;
+        private System.Windows.Forms.Button startArenaBtn;
+        private System.Windows.Forms.Button startVersusBtn;
+        private System.Windows.Forms.Button startChaosBtn;
+        private System.Windows.Forms.Button optionsBtn;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
 
